f91d7421255816bec088f16680555cd1ff8b8125
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2019-05-16 19:36:55 +0100

    Document input variables

--- variables.tf
+++ variables.tf
@@ -1,40 +1,40 @@
 variable "vpc_name" {
     type        = "string"
-    description = ""
+    description = "The name for the VPC"
 }
 
 variable "vpc_cidr" {
     type        = "string"
     default     = "10.0.0.0/16"
-    description = ""
+    description = "The CIDR block for the VPC"
 }
 
 variable "public_subnet_name" {
     type        = "string"
     default     = "Public subnet"
-    description = ""
+    description = "The name for the public subnet"
 }
 
 variable "public_subnet_cidr" {
     type        = "string"
     default     = "10.0.0.0/24"
-    description = ""
+    description = "The CIDR block for the public subnet"
 }
 
 variable "private_subnet_name" {
     type        = "string"
     default     = "Private subnet"
-    description = ""
+    description = "The name for the private subnet"
 }
 
 variable "private_subnet_cidr" {
     type        = "string"
     default     = "10.0.1.0/24"
-    description = ""
+    description = "The CIDR block for the private subnet"
 }
 
 variable "aws_availability_zone" {
     type        = "string"
-    description = ""
+    description = "The availability zone for the public and private subnets"
 }
 
